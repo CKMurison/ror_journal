99834b8ffb17ad48dbf37d955671e09c7312a8ee
Cameron Murison <cmuris4@gmail.com>
2023-12-05 11:34:39 +0000

    removed hello world test method displaying on homepage and replaced them with new ones from a new route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  root 'application#hello'
+root 'entries#index'
+resource :entries, only: [:create, :new]
 end
