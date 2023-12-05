6defe7e098b1e2f40723c88bb57034ff754bd8a8
Cameron Murison <cmuris4@gmail.com>
2023-12-05 11:40:40 +0000

    modified methods in entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,10 +1,25 @@
 class EntriesController < ApplicationController
-    def index 
-    end
 
+    def index
+        @entries = Entry.all
+    end
+ 
     def new
+        @entry = Entry.new()
     end
-
+ 
     def create
+        @entry = Entry.new(entry_params)
+        if @entry.save
+            redirect_to root_url
+        else
+            render :new
+        end
     end
-end
+ 
+    private
+ 
+    def entry_params
+        params.require(:entry).permit(:name)
+    end
+ end
