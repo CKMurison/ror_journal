3b57ec231c2ae39a7a5d4f27ae26f1af93093bbb
Cameron Murison <cmuris4@gmail.com>
2023-12-11 15:44:23 +0000

    added and debugged delete button, now working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,25 +1,31 @@
 class EntriesController < ApplicationController
 
     def index
-        @entries = Entry.all
+      @entries = Entry.all
     end
- 
+  
     def new
-        @entry = Entry.new()
+      @entry = Entry.new()
     end
- 
+  
     def create
-        @entry = Entry.new(entry_params)
-        if @entry.save
-            redirect_to root_url
-        else
-            render :new
-        end
+      @entry = Entry.new(entry_params)
+      if @entry.save
+        redirect_to root_url
+      else
+        render :new
+      end
+    end 
+  
+    def destroy
+      Entry.find(params[:id]).destroy
+      redirect_to root_url
     end
- 
+  
     private
- 
+  
     def entry_params
-        params.require(:entry).permit(:name)
+      params.require(:entry).permit(:name)
     end
- end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'entries#index'
-  resources :entries, only: [:create, :new]
+  resources :entries, only: [:create, :new, :destroy]
 end
